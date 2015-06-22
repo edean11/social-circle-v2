1b4460efe5b0c6e0b067307df5011b3b4af73fcf
thedean11 <edean1010@gmail.com>
2015-06-22 00:31:07 -0500

    add and remove subscription in js working

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,25 +5,15 @@ class SubscriptionsController < ApplicationController
   end
 
   def create
-    group_ids = params[:subscription][:groups][:group_id]
-    error_arr = []
-    group_ids.each do |group_id|
-        if !group_id.empty?
-            @group = Group.find(group_id)
-            subscription = Subscription.create(:user_id => current_user.id, :group_id => @group.id)
-            error_arr.push(subscription) if !subscription.valid?
-        end
-    end
-    if error_arr.empty?
-      message = "Subscription(s) Created Successfully!"
-      redirect_to user_path(current_user), notice: message
-    else
-      flash.alert = "Your subscription(s) could not be created. Please correct the errors below."
-      render :new
-    end
+    group_id = params[:group_id]
+    @group = Group.find(group_id)
+    subscription = Subscription.create(:user_id => current_user.id, :group_id => @group.id)
   end
 
   def destroy
+    subscription_id = params[:id]
+    subscription = Subscription.find_by(id: subscription_id)
+    subscription.destroy
   end
 
 end
