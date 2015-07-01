75c6aad0443c40219400f51c04211ce0cf535336
thedean11 <edean1010@gmail.com>
2015-06-30 20:54:47 -0500

    seed images

--- spec/fabricators/cast_fabricator.rb
+++ spec/fabricators/cast_fabricator.rb
@@ -3,7 +3,7 @@ Fabricator(:cast) do
   user(fabricator: :user)
   title   { Faker::Lorem.words(2).join(" ") }
   content  { Faker::Lorem.words(4).join(" ") }
-  day_num = Random.rand(0.005...90).to_i
+  day_num = Random.rand(0.005...60).to_i
   future_date = (day_num).days.from_now.to_s
   expiration { DateTime.strptime(((Random.rand(2...90).to_i).days.from_now.to_s), '%Y-%m-%d %H:%M:%S %z').to_s }
   lat {Random.rand(30.06909396443887...48.77791275550184)}
